7818b9d89c65fcfd47ba61b6af02a3c4629609bb
Chris Rico <chris.r.rico@gmail.com>
2020-05-28 21:38:37 -0700

    starting code for sql

---
+++ employee.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
